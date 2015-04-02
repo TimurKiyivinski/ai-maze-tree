39e4abcedcb19862c457b80e49d3f60483237ed1
Timur Lavrenti Kiyivinski <deathseclipse@gmail.com>
2015-04-02 14:37:12 +0800

    I must have been drunk

--- space.cpp
+++ space.cpp
@@ -1,18 +1,27 @@
 #include "space.h"
 
 Space::Space():
-    x(0), y(0)
+    x(0), y(0), type('#')
 {
 }
 
 Space::Space(int X, int Y):
-    x(X), y(Y)
+    x(X), y(Y), type('#')
+{
+}
+
+Space::Space(int X, int Y, char _type):
+    x(X), y(Y), type(_type)
 {
 }
 
 Space::~Space()
 {
 }
+
+Space::is_start(){ return type == 's' || type == 'S';}
+
+Space::is_finish(){ return type == 'f' || type == 'F';}
 
 bool Space::is_parent(Space *s)
 {
--- space.h
+++ space.h
@@ -1,18 +1,26 @@
 class Space;
 class Space
 {
-    public:
+    private:
         // X position
         int x;
         // Y position
         int y;
+        // Start/Fin char
+        char type;
+    public:
         // Empty constructor
         Space();
         // X, Y constructor
         Space(int x, int y);
+        // X, Y, Start/Fin Constructor
+        Space(int x, int y, char _type);
         // Destructor
         ~Space();
-    private:
+        // Returns type if type is start
+        bool is_start();
+        // Returns type if type is finish
+        bool is_finish();
         // Returns true if a Space is a parent node
         bool is_parent(Space* s);
         // Adds Space as child node in tree
